8aa0d89ab17a044d8ed881e0dcbfaa1509a74e4a
August <939656378@qq.com>
2022-02-11 22:34:56 +0800

    git Test

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author August
+ * @date 2022/2/11 - 22:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
