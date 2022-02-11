d1c66fb92f153bb1cd1a2d0d13b82dddc9868806
zhangaugust2020 <63038701+zhangaugust2020@users.noreply.github.com>
2022-02-11 23:02:27 +0800

    pulltest

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("test");
+        System.out.println("pull test");
     }
 }
